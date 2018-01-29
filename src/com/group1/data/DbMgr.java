7ddb09e5b49925ec34a195214f86b0b2ceee7349
AthiraSPillai <30954729+AthiraSPillai@users.noreply.github.com>
2018-01-29 12:18:55 -0500

    Update DbMgr.java

--- src/com/group1/data/DbMgr.java
+++ src/com/group1/data/DbMgr.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbMgr {
-	public static Connection getConnection() throws URISyntaxException, SQLException {
+	public static Connection getConnection() throws ClassNotFoundException, SQLException {
     String dbUrl = System.getenv("JDBC_DATABASE_URL");
  this.connection = DriverManager.getConnection(dbUrl);
 		return connection;
