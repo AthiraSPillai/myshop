db768a2762d3720b680f253766414147a3d25f0d
AthiraSPillai <30954729+AthiraSPillai@users.noreply.github.com>
2018-01-29 12:21:13 -0500

    Update DbMgr.java

--- src/com/group1/data/DbMgr.java
+++ src/com/group1/data/DbMgr.java
@@ -5,9 +5,26 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DbMgr {
-	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-    String dbUrl = System.getenv("JDBC_DATABASE_URL");
- this.connection = DriverManager.getConnection(dbUrl);
+	private String userName;
+	private String password;
+	private String url;
+	private String className;
+	
+	private Connection connection ;
+	public DbMgr(String userName, String password, String url, String className) throws ClassNotFoundException, SQLException {
+		super();
+		this.userName = userName;
+		this.password = password;
+		this.url = url;
+		this.className = className;
+		Class.forName(className);
+		String dbUrl = System.getenv("JDBC_DATABASE_URL");
+		this.connection =
+				DriverManager.getConnection(dbUrl);
+		
+	}
+	
+	public Connection getConnection(){
 		return connection;
 	}
 	
